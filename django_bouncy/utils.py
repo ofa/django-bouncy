c6b089d16a0c6f36baf8c055187cc3576b5e4856
tell-k <ffk2005@gmail.com>
2017-02-13 16:24:30 +0900

    fxied python3 support.

--- django_bouncy/utils.py
+++ django_bouncy/utils.py
@@ -27,7 +27,7 @@ from django.conf import settings
 from django.core.cache import caches
 from django.http import HttpResponse, HttpResponseBadRequest
 from django.utils import timezone
-from django.utils.encoding import smart_str
+from django.utils.encoding import smart_bytes
 import dateutil.parser
 
 from django_bouncy import signals
@@ -79,7 +79,7 @@ def grab_keyfile(cert_url):
         pemfile = response.read()
         # Extract the first certificate in the file and confirm it's a valid
         # PEM certificate
-        certificates = pem.parse(smart_str(pemfile))
+        certificates = pem.parse(smart_bytes(pemfile))
 
         # A proper certificate file will contain 1 certificate
         if len(certificates) != 1:
